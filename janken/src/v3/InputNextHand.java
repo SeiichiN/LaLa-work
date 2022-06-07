5d4d4538ef522ee9e1c576624b1496659289996e
SeiichiN <billie175@gmail.com>
2022-06-07 10:54:03 +0900

    InputNextHand.java

--- janken/src/v3/InputNextHand.java
+++ janken/src/v3/InputNextHand.java
@@ -1,14 +1,24 @@
 package v3;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class InputNextHand implements NextHand {
 	public int decideHand() {
-		System.out.println("手を選択してください。");
-		System.out.println("0:グー 1:ちょき 2:パー");
-		System.out.print(">");
-		@SuppressWarnings("resource")
-		int hand = new Scanner(System.in).nextInt();
+        @SuppressWarnings("resource")
+        int hand = -1;
+        do {
+            System.out.println("手を選択してください。");
+            System.out.print("0:グー 1:ちょき 2:パー");
+            System.out.print(" > ");
+            try {
+                hand = new Scanner(System.in).nextInt();
+            } catch (InputMismatchException e) {
+                e.printStackTrace();
+                System.out.println("数字を入力してください。");
+                hand = -1;
+            }
+        } while (hand < 0 || hand > 2);
 		return hand;
 	}
 
